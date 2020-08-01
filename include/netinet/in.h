3ddae3973e1b6cdaa9be2f55967460882b8fefeb
Specter <cryptogenicdev@gmail.com>
2020-08-01 14:22:20 -0700

    Fix sockaddr_in struct + AF_INET6 adjustment

--- include/netinet/in.h
+++ include/netinet/in.h
@@ -13,12 +13,22 @@ typedef uint16_t in_port_t;
 typedef uint32_t in_addr_t;
 struct in_addr { in_addr_t s_addr; };
 
+#ifdef PS4
+struct sockaddr_in {
+	uint8_t sin_len;
+	sa_family_t sin_family;
+	in_port_t sin_port;
+	struct in_addr sin_addr;
+	uint8_t sin_zero[8];
+};
+#else
 struct sockaddr_in {
 	sa_family_t sin_family;
 	in_port_t sin_port;
 	struct in_addr sin_addr;
 	uint8_t sin_zero[8];
 };
+#endif
 
 struct in6_addr {
 	union {
